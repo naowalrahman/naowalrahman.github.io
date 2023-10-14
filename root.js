bb0a92a8c3f4be992f2be32fea62b7919a0140b5
Naowal Rahman <naowal.ar@gmail.com>
2023-10-14 15:18:09 -0400

    furnish website and add projects page

--- root.js
+++ root.js
@@ -1,7 +1,7 @@
 var navbar = `
-<nav class="navbar navbar-expand-lg navbar-fixed-top">
+<nav class="navbar navbar-expand-lg navbar-fixed-top h3 py-4">
         <div class="container">
-            <a class="navbar-brand" href="/index">Naowal Rahman</a>
+            <a class="navbar-brand" style="font-size: larger !important;" href="/index">Naowal Rahman</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -9,10 +9,10 @@ var navbar = `
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav d-flex">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Projects</a>
+                    <li class="nav-item ms-5">
+                        <a class="nav-link" href="../projects">Projects</a>
                     </li>
-                    <li class="nav-item dropdown">
+                    <li class="nav-item dropdown ms-5">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                             aria-expanded="false">Blog</a>
                         <ul class="dropdown-menu">
@@ -21,7 +21,7 @@ var navbar = `
                             <li><a class="dropdown-item" href="../archive-blog/">Archive</a></li>
                         </ul>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item ms-5">
                         <a href="#" class="nav-link">Contact</a>
                     </li>
                 </ul>
